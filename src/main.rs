1b33a3a82a431005fee75d9c978f9227159fa991
Taylor Hornby <taylor@defuse.ca>
2020-05-18 00:53:40 -0600

    Improvements to the code and some new features

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::path::Path;
 use std::fs::File;
 use std::io::BufWriter;
+use clap::{Arg, App};
 
 fn load_wav_to_stereo(filename: &str) -> (Vec<f32>, Vec<f32>) {
     let mut reader = hound::WavReader::open(filename).unwrap();
@@ -61,61 +62,11 @@ impl ToImagePoint for Complex<f32> {
         // FIXME: all this casting is probably broken in several ways
         Point::new(
             (self.re / 2.0 * (width as f32) + (width as f32)/2.0) as i32, 
-            // HACK: using width to scale so it takes up the full screen horizontally, but doesn't distort
+            // HACK: using width to scale so it takes up the full screen horizontally, but doesn't stretch
             (self.im / 2.0 * (width as f32) + (height as f32)/2.0) as i32
         )
     }
 }
-
-/*
-struct RGB {
-    r: u8,
-    g: u8,
-    b: u8
-}
-
-struct Image {
-    pixels: Vec<Vec<RGB>>
-}
-
-impl Image {
-    pub fn new(width: usize, height: usize) {
-        let mut columns: Vec<Vec<RGB>> = Vec::new();
-        for x in 0..width {
-            let mut row: Vec<RGB> = Vec::new();
-            for y in 0..height {
-                row.push(RGB {r: 0, g: 0, b: 0})
-            }
-            columns.push(row)
-        }
-    }
-
-    pub fn set(&mut self, x: usize, y: usize, color: RGB) {
-        self.pixels[x][y] = color;
-    }
-
-    pub fn get(&self, x: usize, y: usize) -> RGB {
-        self.pixels[x][y]
-    }
-
-
-}
-
-fn draw_frame(image: TODO, path: &str) {
-    let path = Path::new(path);
-    let file = File::create(path).unwrap();
-    let ref mut w = BufWriter::new(file);
-
-    let mut encoder = png::Encoder::new(w, image.width(), image.height());
-    encoder.set_color(png::ColorType::RGBA);
-    encoder.set_depth(png::BitDepth::Eight);
-    let mut writer = encoder.write_header().unwrap();
-
-    let data = image.to_rgba();
-    writer.write_image_data(&data).unwrap();
-}
-
-*/
 
 fn find_sdl_gl_driver() -> Option<u32> {
     for (index, item) in sdl2::render::drivers().enumerate() {
@@ -127,6 +78,68 @@ fn find_sdl_gl_driver() -> Option<u32> {
 }
 
 fn main() {
+    let matches = App::new("fft-anything")
+        .version("0.1.0")
+        .author("Taylor Hornby <taylor@defuse.ca>")
+        .about("Generate fourier transform visualizations from any 44.1kHz stereo WAV file")
+        .arg(Arg::with_name("waveform")
+            .short("-w")
+            .long("waveform")
+            .takes_value(false)
+            .help("Draw the waveform at the bottom (slow)"))
+        .arg(Arg::with_name("savepngs")
+            .short("-p")
+            .long("savepngs")
+            .takes_value(true)
+            .help("Path to directory (will be created) to save animation frame .png images in"))
+        .arg(Arg::with_name("num-harmonics")
+            .short("-n")
+            .long("num-harmonics")
+            .takes_value(true)
+            .help("The number of harmonics to draw (default 1000)"))
+        .arg(Arg::with_name("zoom")
+            .short("-z")
+            .long("zoom")
+            .takes_value(true)
+            .help("Scale the the animation by this factor (default 2.0)"))
+        .arg(Arg::with_name("raw-vectors")
+            .short("-r")
+            .long("raw-vectors")
+            .takes_value(false)
+            .help("Draw the positive frequency vectors from the center, too"))
+        .arg(Arg::with_name("speed")
+            .short("-s")
+            .long("speed")
+            .takes_value(true)
+            .help("Animation speed factor (default 0.02)"))
+        // TODO: implement start time and end time by slicing that section out of the file
+        //.arg(Arg::with_name("start-time")
+        //    .short("-s")
+        //    .long("start-time")
+        //    .takes_value(true)
+        //    .help("Start time (in seconds)"))
+        //.arg(Arg::with_name("end-time")
+        //    .short("-e")
+        //    .long("end-time")
+        //    .takes_value(true)
+        //    .help("End time (in seconds)"))
+        .arg(Arg::with_name("input")
+            .multiple(false))
+        .get_matches();
+    
+    let slow_draw_waveform = matches.is_present("waveform");
+    let save_dir : Option<&str> = matches.value_of("savepngs");
+    let num_harmonics : usize = matches.value_of("num-harmonics").unwrap_or("1000").parse().unwrap();
+    let scale : f32 = matches.value_of("zoom").unwrap_or("2.0").parse().unwrap();
+    let speed_factor : f32 = matches.value_of("speed").unwrap_or("0.02").parse().unwrap();
+    let raw_vectors = matches.is_present("raw-vectors");
+    let input_path = matches.value_of("input").unwrap();
+    //let start_time : f32 = matches.value_of("start-time").unwrap_or("0.0").parse().unwrap();
+    //let end_time : Option<f32> = if matches.is_present("end-time") {
+    //    Some(matches.value_of("end-time").unwrap().parse().unwrap())
+    //} else {
+    //    None
+    //};
 
     let running = Arc::new(AtomicBool::new(true));
     let r = running.clone();
@@ -136,14 +149,12 @@ fn main() {
     }).expect("Error setting Ctrl-C handler");
 
     let sdl_context = sdl2::init().unwrap();
-    //let image_width = 1920;
-    //let image_height = 1080;
-    let image_width =  1280;
-    let image_height = 720;
+    let image_width =  1920;
+    let image_height = 1080;
     
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem.window("Animation", image_width, image_height)
-        .opengl() // this line DOES NOT enable opengl, but allows you to create/get an OpenGL context from your window.
+        .opengl() 
         .build()
         .unwrap();
     let mut canvas = window.into_canvas()
@@ -151,26 +162,7 @@ fn main() {
         .build()
         .unwrap();
 
-    canvas.set_draw_color(Color::RGB(0, 0, 0));
-    // fills the canvas with the color we set in `set_draw_color`.
-    canvas.clear();
-
-    // change the color of our drawing with a gold-color ...
-    canvas.set_draw_color(Color::RGB(255, 210, 0));
-    // A draw a rectangle which almost fills our window with it !
-    canvas.fill_rect(Rect::new(10, 10, 780, 580)).unwrap();
-
-    // However the canvas has not been updated to the window yet,
-    // everything has been processed to an internal buffer,
-    // but if we want our buffer to be displayed on the window,
-    // we need to call `present`. We need to call this everytime
-    // we want to render a new frame on the window.
-    canvas.present();
-
-
-    let input_mono = load_wav_to_mono("4s.wav");
-    let max = input_mono.iter().cloned().fold(0./0., f32::max);
-    print!("max: {}", max);
+    let input_mono = load_wav_to_mono(input_path);
     let mut input: Vec<Complex<f32>> = input_mono.iter().cloned().map(|x| Complex::new(x, 0.0)).collect();
     let mut output: Vec<Complex<f32>> = vec![Complex::zero(); input.len()];
 
@@ -181,20 +173,8 @@ fn main() {
     let seconds = input.len() as f32 / 44100.0;
     let fundamental = 1.0 / seconds;
 
-    /*
-    print!("Seconds: {}\n", seconds);
-    for i in 0..output.len() {
-        print!("{} Hz: {}, {}\n", i as f32 * fundamental, output[i].norm(), output[i].arg() / std::f32::consts::PI * 180.0);
-    }
-    */
-    // let num_frames = (seconds * target_fps / speed_factor) as u64;
-
-    let speed_factor = 0.02;
     let target_fps = 60.0;
     let frame_duration_ms = (1000.0 / target_fps) as u64;
-    let scale : f32 = 2.0;
-
-    assert!(output.len() % 2 == 0);
 
     // https://github.com/Rust-SDL2/rust-sdl2/blob/master/examples/ttf-demo.rs
     let texture_creator = canvas.texture_creator();
@@ -202,13 +182,17 @@ fn main() {
     let mut font = ttf_context.load_font("./DejaVuSans-Bold.ttf", 128).unwrap();
     font.set_style(sdl2::ttf::FontStyle::BOLD);
 
-    let render = true;
+    // TODO: print the fundmaental frequency so the user knows what they're looking at
+
+    if save_dir.is_some() {
+            std::fs::create_dir(save_dir.unwrap()).unwrap();
+    }
 
     let mut frame: u64 = 0;
     while running.load(Ordering::SeqCst) {
         let simulated_time_s = (frame * frame_duration_ms) as f32 / 1000.0 * speed_factor;
 
-        if simulated_time_s > seconds {
+        if simulated_time_s > seconds /* || (end_time.is_some() && simulated_time_s > end_time.unwrap() )*/ {
             break;
         }
 
@@ -222,36 +206,36 @@ fn main() {
         canvas.copy(&texture, None, Some(Rect::new(100, 100, 150, 50))).unwrap();
 
         // Draw the status bar
-        if true {
-        let status_bar_height = 100;
-        //      draw the waveform
-        for (t, sample) in input_mono.iter().enumerate() {
-            let x = (t as f32) / (input_mono.len() as f32) * (image_width as f32);
-            let y = image_height as f32 - (status_bar_height as f32)/2.0 + sample * (status_bar_height as f32)/2.0;
-            canvas.set_draw_color(Color::RGB(255, 255, 255));
-            canvas.fill_rect(Rect::new(x as i32, y as i32, 1, 1)).unwrap();
-        }
-        //      draw the cursor
-        canvas.set_draw_color(Color::RGB(255, 255, 0));
-        canvas.fill_rect(Rect::new((simulated_time_s / seconds * (image_width as f32)) as i32, (image_height - 100) as i32, 3, 100)).unwrap();
+        if slow_draw_waveform {
+            let status_bar_height = 100;
+            //      draw the waveform
+            for (t, sample) in input_mono.iter().enumerate() {
+                let x = (t as f32) / (input_mono.len() as f32) * (image_width as f32);
+                let y = image_height as f32 - (status_bar_height as f32)/2.0 + sample * (status_bar_height as f32)/2.0;
+                canvas.set_draw_color(Color::RGB(255, 255, 255));
+                canvas.fill_rect(Rect::new(x as i32, y as i32, 1, 1)).unwrap();
+            }
+            //      draw the cursor
+            canvas.set_draw_color(Color::RGB(255, 255, 0));
+            canvas.fill_rect(Rect::new((simulated_time_s / seconds * (image_width as f32)) as i32, (image_height - 100) as i32, 3, 100)).unwrap();
         }
 
         // Start at the origin.
         let mut last_pos : Complex<f32> = Complex::zero();
 
-        // Draw the harmonic vectors themselves in the background
+        if raw_vectors {
+            // Draw the positive frequency harmonic vectors themselves in the background
+            for i in 0..num_harmonics {
+                let frequency = fundamental * (i as f32);
+                let harmonic = scale * output[i] / (output.len() as f32) * (Complex::i() * frequency * simulated_time_s * std::f32::consts::PI * 2.0).exp();
 
-        for i in 0..1000 {
-            let frequency = fundamental * (i as f32);
-            let harmonic = scale * output[i] / (output.len() as f32) * (Complex::i() * frequency * simulated_time_s * std::f32::consts::PI * 2.0).exp();
-
-            // Aside: draw the harmonic vector itself
-            canvas.set_draw_color(Color::RGB(100, 100, 100));
-            canvas.draw_line(Complex::zero().to_image_point(image_width, image_height), (1.0*harmonic).to_image_point(image_width, image_height)).unwrap();
+                canvas.set_draw_color(Color::RGB(100, 100, 100));
+                canvas.draw_line(Complex::zero().to_image_point(image_width, image_height), (1.0*harmonic).to_image_point(image_width, image_height)).unwrap();
+            }
         }
 
-        // Draw only the lowest 1000 harmonics.
-        for i in 0..1000 {
+        // Positive frequencies
+        for i in 0..num_harmonics {
             let frequency = fundamental * (i as f32);
             let harmonic = scale * output[i] / (output.len() as f32) * (Complex::i() * frequency * simulated_time_s * std::f32::consts::PI * 2.0).exp();
             let cur_pos : Complex<f32> = last_pos + harmonic;
@@ -265,7 +249,7 @@ fn main() {
 
         // Negative frequencies
         canvas.set_draw_color(Color::RGB(200, 0, 200));
-        for i in 1..1000 {
+        for i in 1..num_harmonics {
             let frequency = -fundamental * (i as f32);
             let harmonic = scale * output[output.len() - i] / (output.len() as f32) * (Complex::i() * frequency * simulated_time_s  * std::f32::consts::PI * 2.0).exp();
             let cur_pos : Complex<f32> = last_pos + harmonic;
@@ -276,7 +260,7 @@ fn main() {
             last_pos = cur_pos;
         }
 
-        // highlight the final point
+        // Highlight the final point
         let last_pos_point = last_pos.to_image_point(image_width, image_height);
         canvas.set_draw_color(Color::RGB(255, 255, 0));
         canvas.fill_rect(
@@ -287,10 +271,12 @@ fn main() {
         ).unwrap();
 
         canvas.present();
-        if render {
+
+        if save_dir.is_some() {
+            // Save to frames/<num>.png
             let pixels = canvas.read_pixels(Rect::new(0, 0, image_width, image_height), sdl2::pixels::PixelFormatEnum::RGB24).unwrap();
 
-            let filename = format!("frames/{:06}.png", frame);
+            let filename = format!("{}/{:06}.png", save_dir.unwrap(), frame);
             let path = Path::new(&filename);
             let file = File::create(path).unwrap();
             let ref mut w = BufWriter::new(file);
@@ -299,7 +285,6 @@ fn main() {
             encoder.set_depth(png::BitDepth::Eight);
             let mut writer = encoder.write_header().unwrap();
             writer.write_image_data(&pixels).unwrap();
-            
         } else {
             std::thread::sleep(std::time::Duration::from_millis(frame_duration_ms));
         }
